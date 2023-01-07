be953fc406b436b3e04974c2404f08b8a101e122
Dener <denerbarretocosta@gmail.com>
2023-01-07 11:59:10 -0300

    test: add validation for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(3).is_at_most(50) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should allow_value("email@example.com").for(:email) }
+    it { should_not allow_value("invalid_email").for(:email) }
+    it { should have_secure_password }
+    it { should validate_presence_of(:password) }
+    it { should validate_confirmation_of(:password) }
+    it { should validate_length_of(:password).is_at_least(8).is_at_most(20) }
+    it { should validate_presence_of(:password_digest) }
+  end
 end
