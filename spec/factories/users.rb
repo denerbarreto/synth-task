e3d3254e1d0bb468168c579fc2efd0a3e04baa5f
Dener <denerbarretocosta@gmail.com>
2023-01-07 12:00:08 -0300

    test: add user model factory tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,8 @@
 FactoryBot.define do
   factory :user do
-    name { "MyString" }
-    email { "MyString" }
-    password_digest { "MyString" }
-    tokens { "" }
-    reset_password_tokens { "" }
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(min_length: 8, max_length: 20) }
+    password_confirmation { password }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,64 @@ RSpec.describe User, type: :model do
     it { should validate_length_of(:password).is_at_least(8).is_at_most(20) }
     it { should validate_presence_of(:password_digest) }
   end
+
+  describe "user model factory tests" do
+    it "creates a valid user" do
+      user = create(:user)
+      expect(user).to be_valid
+    end
+
+    it "saves a user to the database" do
+      user = build(:user)
+      expect { user.save }.to change(User, :count).by(1)
+    end
+
+    it "validates presence of name" do
+      user = build(:user, name: nil)
+      expect(user).to be_invalid
+    end
+
+    it "validates length of name" do
+      user = build(:user, name: 'Ab')
+      expect(user).to be_invalid
+      
+      user.name = 'A' * 51
+      expect(user).to be_invalid
+    end
+
+    it "validates presence of email" do
+      user = build(:user, email: nil)
+      expect(user).to be_invalid
+    end
+
+    it "validates uniqueness of email" do
+      create(:user, email: 'user@example.com')
+      user = build(:user, email: 'user@example.com')
+      expect(user).to be_invalid
+    end
+
+    it "validates format of email" do
+      user = build(:user, email: 'invalid_email')
+      expect(user).to be_invalid
+    end
+
+    it "validates presence of password" do
+      user = build(:user, password: nil)
+      expect(user).to be_invalid
+    end
+
+    it "validates presence of password confirmation" do
+      user = build(:user, password_confirmation: nil)
+    end
+
+    it "creates a user with a password that meets the minimum length requirement" do
+      user = build(:user, password: "short")
+      expect(user).to_not be_valid
+    end
+
+    it "creates a user with a password that meets the maximum length requirement" do
+      user = build(:user, password: "thispasswordiswaytoolongandwillnotwork")
+      expect(user).to_not be_valid
+    end
+  end
 end
