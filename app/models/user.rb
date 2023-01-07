d41670aea4499129a6ddc0f0a2fe4e597f4bf578
Dener <denerbarretocosta@gmail.com>
2023-01-07 11:59:37 -0300

    feat: add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :password, length: { minimum: 8, maximum: 20 }, presence: true
+  validates :password_digest, presence: true
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
