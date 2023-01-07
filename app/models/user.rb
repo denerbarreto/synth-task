f5ff21d4abdf048ecbb5988b18df15f4d9ce5e49
Dener <denerbarretocosta@gmail.com>
2023-01-07 12:03:53 -0300

    feat: user model auth token

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ApplicationRecord
   validates :password_digest, presence: true
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+
+  def generate_auth_token
+    expiry = Time.now + 7.days
+    payload = { user_id: id, username: name, expiry: expiry }
+    token = JWT.encode(payload, Rails.application.secrets.secret_key_base, 'HS256')
+
+    self.tokens << token
+    save
+    return token
+  end
 end
