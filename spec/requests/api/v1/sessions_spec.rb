5d4525fb3e085cadc1abbfa2037ea671f7e909e9
Dener <denerbarretocosta@gmail.com>
2023-01-11 20:03:27 -0300

    test: add test for user session delete

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -55,5 +55,29 @@ RSpec.describe "Api::V1::Sessions", type: :request do
         expect(response.headers["Content-Type"]).to eq("application/vnd.api+json; charset=utf-8")
       end
     end
+
+    describe "DELETE /api/v1/sessions" do
+      let(:user) { create(:user) }
+      let(:credentials) { { email: user.email, password: user.password } }
+
+  
+      it "It allows the user to log out when they are logged in" do
+        post api_v1_sessions_path, params: {
+          "data": {
+            "type": :user,
+            "attributes": credentials
+          }
+        }
+        expect(response).to have_http_status(200)
+
+        auth_token = response.headers["Authorization"].split(' ').last
+        expect(user.reload.tokens).to include(auth_token)
+
+        delete api_v1_session_path(user.id), headers: { "Authorization" => "Bearer #{auth_token}" }
+        expect(response).to have_http_status(200)
+        
+        expect(user.reload.tokens).to_not include(auth_token)
+      end
+    end
   end
 end
