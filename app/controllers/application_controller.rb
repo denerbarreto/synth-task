5a2a96a796d4b1377856d461a8c5a676cc6fd7b8
Dener <denerbarretocosta@gmail.com>
2023-01-11 20:00:40 -0300

    feat: add authenticate_request and set current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::API
+  include ActionController::Helpers
+  
+  before_action :authenticate_request
+  helper_method :current_user
+
+  def current_user
+    @current_user
+  end
+
+  private 
+
+  def authenticate_request
+    if request.headers['Authorization'].present?
+      token = request.headers['Authorization'].split(' ').last
+      begin
+        user = User.authenticate(token)
+        if user
+          @current_user = user
+        else
+          render json: { error: 'Token is invalid' }, status: :unauthorized
+        end
+      rescue JWT::DecodeError => e
+        render json: { error: 'Token is invalid' }, status: :unauthorized
+      end
+    else
+      render json: { error: 'Token is missing' }, status: :unauthorized
+    end
+  end  
 end
