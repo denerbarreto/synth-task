3d2419ba9c1268254d3b945a829be0c3b315beeb
Dener <denerbarretocosta@gmail.com>
2023-01-14 19:45:13 -0300

    test: add test for return all task list via GET request to task_lists controller

--- spec/requests/api/v1/task_lists_spec.rb
+++ spec/requests/api/v1/task_lists_spec.rb
@@ -61,4 +61,39 @@ RSpec.describe "Api::V1::TaskLists", type: :request do
       end
     end
   end
+
+  describe "GET /api/v1/task_lists" do
+    let!(:task_lists) { create_list(:task_list, 3, user: user) }
+    let(:another_user) { create(:user) }
+
+    it "should return all task_lists in JSON API format" do
+      get api_v1_task_lists_path, headers: auth_headers
+  
+      expect(response).to have_http_status(200)
+      expect(response.headers["Content-Type"]).to eq("application/vnd.api+json; charset=utf-8")
+  
+      json_response = response.parsed_body
+      expect(json_response).to have_key("data")
+      expect(json_response["data"]).to be_an(Array)
+  
+      json_response["data"].each do |task_list|
+        expect(task_list).to have_key("id")
+        expect(task_list).to have_key("type")
+        expect(task_list).to have_key("attributes")
+        expect(task_list).to have_key("relationships")
+        expect(task_list["attributes"]).to have_key("name")
+        expect(task_list["attributes"]).to have_key("order")
+        expect(task_list["relationships"]["user"]["data"]).to have_key("id")
+      end
+    end
+
+    it "should return only task_lists of the current user" do
+      get api_v1_task_lists_path, headers: auth_headers
+    
+      task_lists_data = response.parsed_body["data"]
+      task_lists_data.each do |task_list_data|
+        expect(task_list_data["relationships"]["user"]["data"]["id"]).to eq(user.id.to_s)
+      end
+    end
+  end
 end
