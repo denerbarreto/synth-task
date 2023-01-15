1b0d67cc370f79fcde97279f2f8be60d2e780201
Dener <denerbarretocosta@gmail.com>
2023-01-14 23:06:13 -0300

    feat: add project model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_14_134655) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_15_015835) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
 
   create_table "task_lists", force: :cascade do |t|
     t.string "name"
@@ -33,5 +41,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_14_134655) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "projects", "users"
   add_foreign_key "task_lists", "users"
 end
