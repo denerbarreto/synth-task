dc67a89cc5ff0bebaaf8c59e79a860875a0af9b9
Dener <denerbarretocosta@gmail.com>
2023-01-15 17:29:09 -0300

    feat: add functionality to update the project of the current user in the API with update controller and routes

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::ProjectsController < ApplicationController
+  before_action :set_task_list, only: [:update] 
+  before_action :authorize_user, only: [:update] 
 
   def index
     projects = Project.where(user_id: current_user.id)
@@ -15,7 +17,25 @@ class Api::V1::ProjectsController < ApplicationController
     end
   end
 
+  def update
+    if @project.update(project_params)
+      render json: @project, status: :ok
+    else
+      render json: @project.errors, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def set_task_list
+    @project = Project.find(params[:id])
+  end
+
+  def authorize_user
+    unless @project.user_id == current_user.id
+      render json: { error: 'Not authorized' }, status: :unauthorized
+    end
+  end
 
   def project_params
     ActiveModelSerializers::Deserialization.jsonapi_parse(params)
