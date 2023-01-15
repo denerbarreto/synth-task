32293f77835bfe8a0503b70c2a1546fa2498264e
Dener <denerbarretocosta@gmail.com>
2023-01-15 17:34:39 -0300

    feat: add functionality to delete the project of the current user in the API with delete controller and routes

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ProjectsController < ApplicationController
-  before_action :set_task_list, only: [:update] 
-  before_action :authorize_user, only: [:update] 
+  before_action :set_task_list, only: [:update, :destroy] 
+  before_action :authorize_user, only: [:update, :destroy] 
 
   def index
     projects = Project.where(user_id: current_user.id)
@@ -20,6 +20,14 @@ class Api::V1::ProjectsController < ApplicationController
   def update
     if @project.update(project_params)
       render json: @project, status: :ok
+    else
+      render json: @project.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @project.destroy
+      render json: { message: 'Task list successfully deleted' }, status: :ok
     else
       render json: @project.errors, status: :unprocessable_entity
     end
