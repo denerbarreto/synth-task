b2eeeb3f94446ede0782d010725991df1c3c317e
Dener <denerbarretocosta@gmail.com>
2023-01-15 23:20:00 -0300

    refactor: change in tests routes from /api/v1/task_list to api/v1/projects/:project_id/task_lists

--- spec/requests/api/v1/task_lists_spec.rb
+++ spec/requests/api/v1/task_lists_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe "Api::V1::TaskLists", type: :request do
   let(:credentials) { { email: user.email, password: user.password } }
   let(:auth_headers) { { "Authorization": "Bearer #{auth_token}" } }
   let(:auth_token) { response.headers["Authorization"].split(' ').last }
+  let(:task_list) { create(:task_list, user: user) }
+  let(:another_task_list) { create(:task_list) }
   let(:task_list_attributes) { attributes_for(:task_list) }
 
   before do
@@ -17,26 +19,12 @@ RSpec.describe "Api::V1::TaskLists", type: :request do
     }
   end
 
-  describe "POST /api/v1/task_lists" do
+  describe "POST /api/v1/:project_id/task_lists" do
     it "should create task_list with valid data" do
-      post api_v1_task_lists_path, headers: auth_headers, params: {
+      post api_v1_project_task_lists_path(project), headers: auth_headers, params: {
         "data": {
           "type": :task_list,
           "attributes": task_list_attributes
-        },
-        "relationships": {
-          "user":{
-            "data": {
-              "type": :user,
-              "id": "#{user.id}",
-            }
-          },
-          "project": {
-            "data": {
-              "type": :project,
-              "id": "#{project.id}",
-            } 
-          }
         }
       }
 
@@ -48,24 +36,10 @@ RSpec.describe "Api::V1::TaskLists", type: :request do
       let(:task_list_attributes) { attributes_for(:task_list, name: nil) }
       
       it "should return status code 422" do
-        post api_v1_task_lists_path, headers: auth_headers, params: {
+        post api_v1_project_task_lists_path(project), headers: auth_headers, params: {
           "data": {
             "type": :task_list,
             "attributes": task_list_attributes
-          },
-          "relationships": {
-            "user":{
-              "data": {
-                "type": :user,
-                "id": "#{user.id}",
-              }
-            },
-            "project": {
-              "data": {
-                "type": :project,
-                "id": "#{project.id}",
-              } 
-            }
           }
         }
   
@@ -75,33 +49,11 @@ RSpec.describe "Api::V1::TaskLists", type: :request do
     end
   end
 
-  describe "GET /api/v1/task_lists" do
+  describe "GET /api/v1/:project_id/task_lists" do
     let!(:task_lists) { create_list(:task_list, 3, user: user, project: project) }
 
-    it "should return all task_lists in JSON API format" do
-      get api_v1_task_lists_path, headers: auth_headers
-  
-      expect(response).to have_http_status(200)
-      expect(response.headers["Content-Type"]).to eq("application/vnd.api+json; charset=utf-8")
-  
-      json_response = response.parsed_body
-      expect(json_response).to have_key("data")
-      expect(json_response["data"]).to be_an(Array)
-  
-      json_response["data"].each do |task_list|
-        expect(task_list).to have_key("id")
-        expect(task_list).to have_key("type")
-        expect(task_list).to have_key("attributes")
-        expect(task_list).to have_key("relationships")
-        expect(task_list["attributes"]).to have_key("name")
-        expect(task_list["attributes"]).to have_key("order")
-        expect(task_list["relationships"]["user"]["data"]).to have_key("id")
-        expect(task_list["relationships"]["project"]["data"]).to have_key("id")
-      end
-    end
-
     it "should return only task_lists of the current user" do
-      get api_v1_task_lists_path, headers: auth_headers
+      get api_v1_project_task_lists_path(project), headers: auth_headers
     
       task_lists_data = response.parsed_body["data"]
       task_lists_data.each do |task_list_data|
@@ -110,29 +62,14 @@ RSpec.describe "Api::V1::TaskLists", type: :request do
     end
   end
 
-  describe "PATCH /api/v1/task_lists/:id" do
-    let(:task_list) { create(:task_list, user: user, project: project) }
+  describe "PATCH /api/v1/:project_id/task_lists/:id" do
     let(:another_task_list) { create(:task_list) }
 
     it "should update task_list with valid data and current user" do
-      patch api_v1_task_list_path(task_list), headers: auth_headers, params: {
+      patch api_v1_project_task_list_path(project, task_list), headers: auth_headers, params: {
         "data": {
           "type": :task_list,
           "attributes": { "name": "New Name" }
-        },
-        "relationships": {
-          "user":{
-            "data": {
-              "type": :user,
-              "id": "#{user.id}",
-            }
-          },
-          "project": {
-            "data": {
-              "type": :project,
-              "id": "#{project.id}",
-            } 
-          }
         }
       }
 
@@ -141,24 +78,10 @@ RSpec.describe "Api::V1::TaskLists", type: :request do
     end
 
     it "should not update task_list with invalid data" do
-      patch api_v1_task_list_path(task_list), headers: auth_headers, params: {
+      patch api_v1_project_task_list_path(project, task_list), headers: auth_headers, params: {
         "data": {
           "type": :task_list,
           "attributes": { "name": "" }
-        },
-        "relationships": {
-          "user":{
-            "data": {
-              "type": :user,
-              "id": "#{user.id}",
-            }
-          },
-          "project": {
-            "data": {
-              "type": :project,
-              "id": "#{project.id}",
-            } 
-          }
         }
       }
 
@@ -166,24 +89,10 @@ RSpec.describe "Api::V1::TaskLists", type: :request do
     end
 
     it "should not update task_list of another user" do
-      patch api_v1_task_list_path(another_task_list), headers: auth_headers, params: {
+      patch api_v1_project_task_list_path(project, another_task_list), headers: auth_headers, params: {
         "data": {
           "type": :task_list,
           "attributes": { "name": "New Name" }
-        },
-        "relationships": {
-          "user":{
-            "data": {
-              "type": :user,
-              "id": "#{user.id}",
-            }
-          },
-          "project": {
-            "data": {
-              "type": :project,
-              "id": "#{project.id}",
-            } 
-          }
         }
       }
 
@@ -191,30 +100,12 @@ RSpec.describe "Api::V1::TaskLists", type: :request do
     end
   end
 
-  describe "DELETE /api/v1/task_lists/:id" do
-    let(:task_list) { create(:task_list, user: user, project: project) }
-    let(:another_task_list) { create(:task_list) }
-
+  describe "DELETE /api/v1/:project_id/task_lists/:id" do
     it "should delete task_list of current user" do
-      
-      delete api_v1_task_list_path(task_list), headers: auth_headers, params: {
+      delete api_v1_project_task_list_path(project, task_list), headers: auth_headers, params: {
         "data": {
           "type": :task_list,
-          "attributes": { "id": "#{task_list.id}" }
-        },
-        "relationships": {
-          "user":{
-            "data": {
-              "type": :user,
-              "id": "#{user.id}",
-            }
-          },
-          "project": {
-            "data": {
-              "type": :project,
-              "id": "#{project.id}",
-            } 
-          }
+          "attributes": { "id": task_list.id.to_s }
         }
       }
 
@@ -222,24 +113,10 @@ RSpec.describe "Api::V1::TaskLists", type: :request do
     end
 
     it "should not delete task_list of another user" do
-      patch api_v1_task_list_path(another_task_list), headers: auth_headers, params: {
+      patch api_v1_project_task_list_path(project, another_task_list), headers: auth_headers, params: {
         "data": {
           "type": :task_list,
-          "attributes": { "id": "#{another_task_list.id}" }
-        },
-        "relationships": {
-          "user":{
-            "data": {
-              "type": :user,
-              "id": "#{user.id}",
-            }
-          },
-          "project": {
-            "data": {
-              "type": :project,
-              "id": "#{project.id}",
-            } 
-          }
+          "attributes": { "id": another_task_list.id.to_s }
         }
       }
 
@@ -247,30 +124,13 @@ RSpec.describe "Api::V1::TaskLists", type: :request do
     end
   end
 
-  describe "GET /api/v1/task_lists/:id" do
-    let(:task_list) { create(:task_list, user: user, project: project) }
-    let(:another_task_list) { create(:task_list) }
-
+  describe "GET /api/v1/:project_id/task_lists/:id" do
     it "should show task_list of current user" do
       
-      get api_v1_task_list_path(task_list), headers: auth_headers, params: {
+      get api_v1_project_task_list_path(project, task_list), headers: auth_headers, params: {
         "data": {
           "type": :task_list,
-          "attributes": { "id": "#{task_list.id}" }
-        },
-        "relationships": {
-          "user":{
-            "data": {
-              "type": :user,
-              "id": "#{user.id}",
-            }
-          },
-          "project": {
-            "data": {
-              "type": :project,
-              "id": "#{project.id}",
-            } 
-          }
+          "attributes": { "id": task_list.id.to_s }
         }
       }
 
@@ -282,24 +142,10 @@ RSpec.describe "Api::V1::TaskLists", type: :request do
     end
 
     it "should not show task_list of another user" do
-      get api_v1_task_list_path(another_task_list), headers: auth_headers, params: {
+      get api_v1_project_task_list_path(project, another_task_list), headers: auth_headers, params: {
         "data": {
           "type": :task_list,
-          "attributes": { "id": "#{another_task_list.id}" }
-        },
-        "relationships": {
-          "user":{
-            "data": {
-              "type": :user,
-              "id": "#{user.id}",
-            }
-          },
-          "project": {
-            "data": {
-              "type": :project,
-              "id": "#{project.id}",
-            } 
-          }
+          "attributes": { "id": another_task_list.id.to_s }
         }
       }
 
