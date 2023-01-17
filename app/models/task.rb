5896cf1340aa478902c02e11030cf88ed7876884
Dener <denerbarretocosta@gmail.com>
2023-01-16 21:02:07 -0300

    feat: add validations to task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,8 @@
 class Task < ApplicationRecord
   belongs_to :user
   belongs_to :task_list
+
+  validates :name, presence: true, length: {minimum: 3, maximum: 50}
+  validates :status, presence: true, inclusion: { in: ["Em andamento", "Concluída", "Atrasada", "Cancelada"] }
+  validates :description, :priority, presence: true
 end
