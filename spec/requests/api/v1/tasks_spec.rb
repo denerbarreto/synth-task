574343430d969bc4493173f10e6e24b3f276da04
Dener <denerbarretocosta@gmail.com>
2023-01-20 13:44:39 -0300

    test: add test to update the task via PATCH request to task controller

--- spec/requests/api/v1/tasks_spec.rb
+++ spec/requests/api/v1/tasks_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "Api::V1::Tasks", type: :request do
   let(:auth_headers) { { "Authorization": "Bearer #{auth_token}" } }
   let(:auth_token) { response.headers["Authorization"].split(' ').last }
   let(:task_list) { create(:task_list, user: user, project: project) }
-  let(:task) { create(:task, project: project, task_list: task_list) }
+  let(:task) { create(:task, user: user, task_list: task_list) }
   let(:task_attributes) { attributes_for(:task) }
 
   before do
@@ -16,7 +16,7 @@ RSpec.describe "Api::V1::Tasks", type: :request do
 
   describe "POST api/v1/projects/:project_id/task_lists/:task_list_id/tasks" do
     it "should create task with valid data" do
-      post api_v1_project_task_list_tasks_path(project, task_list), headers: auth_headers, params: { task: task_attributes}
+      post api_v1_project_task_list_tasks_path(project, task_list), headers: auth_headers, params: { task: task_attributes }
 
       expect(response).to have_http_status(201)
       expect(response.headers["Content-Type"]).to eq("application/json; charset=utf-8")
@@ -33,7 +33,7 @@ RSpec.describe "Api::V1::Tasks", type: :request do
       end
     end
 
-    describe "GET /api/v1/:project_id/task_lists" do
+    describe "GET /api/v1/:project_id/task_lists/:task_list_id/tasks" do
       let!(:task) { create_list(:task, 3, user: user, task_list: task_list) }
   
       it "should return only task of the current user" do
@@ -45,5 +45,29 @@ RSpec.describe "Api::V1::Tasks", type: :request do
         end
       end
     end
+
+    describe "PATCH /api/v1/:project_id/task_lists/:task_list_id/tasks/:task_id" do
+      let(:another_task) { create(:task) }
+  
+      it "should update task with valid data and current user" do
+        patch api_v1_project_task_list_task_path(project, task_list, task), headers: auth_headers, params: { task: { "name": "New Name" } }
+
+        expect(response).to have_http_status(200)
+        expect(response.parsed_body["task"]["name"]).to eq("New Name")
+      end
+  
+      it "should not update task with invalid data" do
+        patch api_v1_project_task_list_task_path(project, task_list, task), headers: auth_headers, params: { task: { "name": "" } }
+  
+        expect(response).to have_http_status(422)
+      end
+  
+      it "should not update task of another user" do
+        patch api_v1_project_task_list_task_path(project, task_list, another_task), headers: auth_headers, params: { task:  { "name": "New Name" } }
+  
+        expect(response).to have_http_status(401)
+      end
+    end
+
   end
 end
